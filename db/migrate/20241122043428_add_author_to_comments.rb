0adb54ccc8a3bf6f8bf590602c67608c31ce1092
ce <cecillemanalang@gmail.com>
2024-11-23 10:39:59 +0800

    fix comment author

--- db/migrate/20241122043428_add_author_to_comments.rb
+++ db/migrate/20241122043428_add_author_to_comments.rb
@@ -1,5 +1,5 @@
 class AddAuthorToComments < ActiveRecord::Migration[7.2]
   def change
-    add_reference :comments, :author, null: false, foreign_key: { to_table: :users }
+    add_reference :comments, :author, null: true, foreign_key: { to_table: :users }
   end
 end
